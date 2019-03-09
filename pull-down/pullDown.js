abbfac328a7e066000e987d716202af0d7d11642
HTMLhead <mrdoo309@gmail.com>
2019-03-09 16:31:26 +0900

    Style: 컨벤션에 맞게 수정

--- pull-down/pullDown.js
+++ pull-down/pullDown.js
@@ -6,24 +6,23 @@ class PullDown {
 
   init() {
     this.targetDom = document.querySelector(this.elObj.target);
-    this.targetDom.style.transition = `0.3s all`
+    this.targetDom.style.height =  `${this.optObj.height}`;
+    this.targetDom.style.transition = `0.3s all`;
     this.addBtnEvt();
   }
 
   addBtnEvt() {
     const showingBtn = document.querySelector(this.elObj.openBtn);
-    const closingBtn = document.querySelector(this.elObj.closeBtn)
+    const closingBtn = document.querySelector(this.elObj.closeBtn);
     showingBtn.addEventListener('click', () => this.showing());
     closingBtn.addEventListener('click', () => this.closing());
   }
 
   showing() {
     this.targetDom.style.width = `${this.optObj.width}`;
-    this.targetDom.style.height =  `${this.optObj.height}`;
   }
 
   closing() {
     this.targetDom.style.width = `0`;
-    this.targetDom.style.height = `0`;
   }
 }
