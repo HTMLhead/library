73f511c9554c059b5a2d4753a8be318abdc1607f
HTMLhead <mrdoo309@gmail.com>
2019-03-09 15:37:09 +0900

    Feat: pulldown기능의 js파일에 init기능 추가

--- pull-down/pullDown.js
+++ pull-down/pullDown.js
@@ -1,6 +1,13 @@
 class PullDown {
   constructor(elObj, optObj) {
     Object.assign(this, { elObj, optObj });
+    this.init()
+  }
+
+  init() {
+    const showClass = document.createElement('show');
+    showClass.style = `widht:${this.optObj.width}, height:${this.optObj.height}`
+    this.showClass = showClass
   }
 
   
